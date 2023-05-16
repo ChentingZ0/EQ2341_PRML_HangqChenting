daacf12e8abaebb018ac5c4137fe07f1b33b6252
Hang Qin <112768477+hangqkth@users.noreply.github.com>
2023-05-16 22:26:09 +0200

    update

--- assignment3/functionality.py
+++ assignment3/functionality.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def log_likelihood(gauss_dist, x):
+def likelihood(gauss_dist, x):
     px = []
     for i, dist in enumerate(gauss_dist):
         # for one observed data x and distribution d, the probability Pr(x|d)
         px.append(dist.prob(x))
     px = np.array(px)
-    return np.log(px)
+    return px
--- assignment3/verify.py
+++ assignment3/verify.py
@@ -1,7 +1,7 @@
 from PattRecClasses.MarkovChain import MarkovChain
 from PattRecClasses.GaussD import GaussD
 from PattRecClasses.HMM import HMM
-from functionality import log_likelihood
+from functionality import likelihood
 import numpy as np
 
 # define distribution and observed data
@@ -10,16 +10,14 @@ g2 = GaussD(means=np.array([3]), stdevs=np.array([2.0]))
 g = [g1, g2]
 x_Seq = np.array([-0.2, 2.6, 1.3])
 
-logP = log_likelihood(g, x_Seq)
-
-P_scaled = np.e ** (logP - np.max(logP, axis=0))  # normalized
-P = np.e**logP  # non normalized
+pX = likelihood(g, x_Seq)
+pX_scaled = pX / np.max(pX, axis=0)  # normalized
 
 q = np.array([1, 0])
 A = np.array([[0.9, 0.1, 0], [0, 0.9, 0.1]])
 mc = MarkovChain(q, A)
 h = HMM(mc, g)
-alpha_hat, norms = mc.forward(P_scaled)
+alpha_hat, norms = mc.forward(pX_scaled)
 
 np.set_printoptions(precision=3)
 print("\nx =", x_Seq)
@@ -28,6 +26,6 @@ print("\nA = \n", A)
 print("\nalpha_hat =\n", np.array(alpha_hat).T)
 print("\nc =", np.array(norms))
 
-log_prob = h.logprob(P)
+log_prob = h.logprob(pX)
 print("\nP(X = x|λ)=", log_prob)
 
